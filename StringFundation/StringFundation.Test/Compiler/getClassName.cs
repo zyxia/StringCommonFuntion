72e96f8c1b1dc92b42663646dc142f5e103d3566
xiazeye <xiazeye.xd.com>
2020-09-30 17:15:53 +0800

    添加getclassname测试用例

---
+++ StringFundation/StringFundation.Test/Compiler/getClassName.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Unikon.serialize;
+
+namespace Tests
+{
+    public class getClassName
+    {
+        [Test]
+        public void Test1()
+        {
+            var path = Directory.GetCurrentDirectory();
+          
+            Assert.IsTrue("Unikon.serialize.SimpleCompiler"== SimpleCompiler.getFullClassName(path + "/../../../classnametest.txt"));  
+
+        }
+        
+    }
+}
